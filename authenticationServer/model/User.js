4d6121a7e95054a9cb0fad15ab42c264c07a4f83
SahilUpadhye291998 <sahilsu1998@gmail.com>
2020-06-01 20:10:04 +0530

    A base upload

---
+++ authenticationServer/model/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+let User = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  uniqueIdentifier: {
+    // something like rashan card
+    type: String,
+    required: true,
+  },
+  secret: {
+    //password which will be stored in database
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  typeOf: {
+    type: String,
+    required: true,
+  },
+  customerSecret: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = User = mongoose.model("User", User);
---
+++ authenticationServer/routes/api/user.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/add", (req, res) => {
+  console.log(`Adding this to list`);
+});
+
+router.delete("/delete", (req, res) => {
+  console.log(`Deletion will be placed here`);
+});
+
+router.get("/getAll", (req, res) => {
+  console.log(`Get all the Data of customer`);
+});
+
+router.get("/get/:id", (req, res) => {
+  console.log(`Get all ${req.params.id}`);
+});
+
+module.exports = router;
