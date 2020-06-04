ba8e85a50163cc095bd8b4b86864e7aca870b381
SahilUpadhye291998 <sahilsu1998@gmail.com>
2020-06-04 09:30:37 +0530

    Added a comment

--- authenticationServer/server.js
+++ authenticationServer/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const user = require("./routes/api/user");
-const connectionString = require("./secert/secret").connectionString;
+const express = require('express');
+const mongoose = require('mongoose');
+const user = require('./routes/api/user');
+const connectionString = require('./secert/secret').connectionString;
 
 const app = express();
 process.setMaxListeners(0);
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 const db = process.env.MONGO_URI || connectionString;
 
@@ -20,12 +20,13 @@ mongoose
   .then(() => {
     console.log(`Connection to database is done`);
   })
-  .catch((err) => {
+  .catch(err => {
     console.error(`${err}`);
   });
 
-app.use("/api/user", user);
+app.use('/api/user', user);
 const PORT = 3000 || process.env.PORT;
+//starting point of the server is placed here
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
